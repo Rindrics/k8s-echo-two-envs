d1bdc54c6d92594e8ab863b7927ee76a162d36da
Akira Hayashi <smxshxishxad@me.com>
2022-08-29 03:13:11 +0900

    :sparkles: Define Terraform module to use `kustomize`

---
+++ tffile/module/variables.tf
@@ -0,0 +1,9 @@
+variable "context" {
+  type        = string
+  description = "Name of kubectl context"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of environment"
+}
