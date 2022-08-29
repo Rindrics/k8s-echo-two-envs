10988c8ed2244fef2f1ebffd4dc34e5febacfe9a
Akira Hayashi <smxshxishxad@me.com>
2022-08-29 11:16:33 +0900

    :recycle: Load cluster config from file

--- tffile/module/local_infra/main.tf
+++ tffile/module/local_infra/main.tf
@@ -9,24 +9,5 @@ terraform {
 
 resource "kind_cluster" "new" {
   name   = "production"
-  config = <<-EOF
-        kind: Cluster
-        apiVersion: kind.x-k8s.io/v1alpha4
-        name: production
-        nodes:
-        - role: control-plane
-          kubeadmConfigPatches:
-          - |
-            kind: InitConfiguration
-            nodeRegistration:
-              kubeletExtraArgs:
-                node-labels: "ingress-ready=true"
-          extraPortMappings:
-          - containerPort: 80
-            hostPort: 80
-            protocol: TCP
-          - containerPort: 443
-            hostPort: 443
-            protocol: TCP
-    EOF
+  config = file("${path.module}/cluster_config.tftpl")
 }
