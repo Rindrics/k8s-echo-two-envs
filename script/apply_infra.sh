dbf19f4aa0175158f65523e546eed75bfd0d1740
Akira Hayashi <smxshxishxad@me.com>
2022-08-31 03:28:58 +0900

    :sparkles: Deploy metrics server to use `HorizontalPodAutoscaler`

--- script/apply_infra.sh
+++ script/apply_infra.sh
@@ -12,3 +12,10 @@ kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"metrics-server","args":["--cert-dir=/tmp","--secure-port=4443","--v=2","--kubelet-insecure-tls","--kubelet-preferred-address-types=InternalIP"]}]}}}}'
+sleep 10
+kubectl wait --namespace kube-system \
+  --for=condition=ready pod \
+  --selector=k8s-app=metrics-server \
+  --timeout=90s
